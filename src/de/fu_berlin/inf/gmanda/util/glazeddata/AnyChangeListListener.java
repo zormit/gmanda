d16ba73b98a3aeab39ecacd86924262fd9658f9c
Christopher Oezbek <c.oezbek@gmail.com>
2008-07-19 13:07:13 +0000

    Added lots of util code from other projects.
    Added more code to path routines, like line intersection and stuff.
    
    Implemented a first version of Voronoi-finding for pathfinding later on.
    
    git-svn-id: https://svn.code.sf.net/p/gmanda/code/trunk@108 efa6dfe3-884f-0410-b496-8720d998413f

--- src/de/fu_berlin/inf/gmanda/util/glazeddata/AnyChangeListListener.java
+++
@@ -1,7 +0,0 @@
-package de.fu_berlin.inf.gmanda.util.glazeddata;
-
-public interface AnyChangeListListener<T> {
-	
-	public void changed();
-
-}
--- src/de/fu_berlin/inf/gmanda/util/glazeddata/MinimalListListener.java
+++
@@ -1,9 +0,0 @@
-package de.fu_berlin.inf.gmanda.util.glazeddata;
-
-public interface MinimalListListener<T> {
-	
-	public void add(T added);
-	
-	public void remove(T removed);
-
-}
