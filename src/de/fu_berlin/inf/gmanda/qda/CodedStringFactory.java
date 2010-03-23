b606859f41beab8794c049fddb2a6ab4da4845f9
Christopher Oezbek <c.oezbek@gmail.com>
2010-03-23 13:19:13 +0000

    Catch NPE and added some docu
    git-svn-id: https://svn.code.sf.net/p/gmanda/code/trunk@180 efa6dfe3-884f-0410-b496-8720d998413f

--- src/de/fu_berlin/inf/gmanda/qda/CodedStringFactory.java
+++ src/de/fu_berlin/inf/gmanda/qda/CodedStringFactory.java
@@ -5,22 +5,32 @@ import java.util.Iterator;
 import de.fu_berlin.inf.gmanda.qda.tagxon.TagxON;
 
 public class CodedStringFactory {
-	
+
 	static TagxON t = new TagxON();
-	
-	public static CodedString parse(String s){
+
+	/**
+	 * Returns the CodedString using TagxOn notation contained in the given
+	 * string or null if the string does not contain any codes
+	 */
+	public static CodedString parse(String s) {
 		return t.parseCodedString(s);
 	}
 
+	/**
+	 * @return the first code contained in the given String or null if the
+	 *         string does not contain any code.
+	 */
 	public static Code parseOne(String s) {
-		
+
 		CodedString c = t.parseCodedString(s);
-		
+		if (c == null)
+			return null;
+
 		Iterator<? extends Code> it = c.getAllCodes().iterator();
-		
+
 		if (it.hasNext())
 			return it.next();
-		else 
+		else
 			return null;
 	}
 
