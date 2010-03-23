c8307ff8c7b7e08609e44bbb8553748ecfce1316
Christopher Oezbek <c.oezbek@gmail.com>
2010-03-23 13:20:14 +0000

    Reformat and some documentation
    git-svn-id: https://svn.code.sf.net/p/gmanda/code/trunk@181 efa6dfe3-884f-0410-b496-8720d998413f

--- src/de/fu_berlin/inf/gmanda/gui/search/SearchService.java
+++ src/de/fu_berlin/inf/gmanda/gui/search/SearchService.java
@@ -36,64 +36,71 @@ import de.fu_berlin.inf.gmanda.util.progress.IProgress;
  * 
  * for Metadata use:
  * 
- * search: {
- *   from: "Bogdan",
- *   OR from: "Aleksandr"
- * }
+ * search: { from: "Bogdan", OR from: "Aleksandr" }
  * 
  * for Date based filtering use:
  * 
  * date: <2007-04-01T12:00:00
  * 
- * or 
+ * or
  * 
  * date: >2007-05-01
  */
 public class SearchService {
-	
+
 	private static final Logger log = Logger.getLogger(SearchService.class);
 
 	@Inject
 	LuceneFacade luceneFacade;
-	
+
 	@Inject
 	CommonService commonService;
-	
+
 	@Inject
 	SearchStringProxy searchStringProxy;
 
-	public List<PrimaryDocument> filter(final String filterString, @Nullable
-		IProgress pBar, Project project) {
-		Iterable<? extends Code> codesIterable = CodedStringFactory.parse(filterString)
-			.getAllCodes();
+	/**
+	 * Given a Project and a string to filter for, this method returns a list of
+	 * all those primary documents which are selected by the filterString
+	 */
+	public List<PrimaryDocument> filter(final String filterString,
+			@Nullable IProgress pBar, Project project) {
+		Iterable<? extends Code> codesIterable = CodedStringFactory.parse(
+				filterString).getAllCodes();
 
 		if (pBar == null)
 			pBar = commonService.getProgressBar("Filtering");
-		
+
 		IProgress subProgress = pBar.getSub(100);
 
 		TreeSet<PrimaryDocument> newFilterList;
 
 		try {
-			subProgress.setScale(CollectionUtils.size(codesIterable.iterator()));
+			subProgress
+					.setScale(CollectionUtils.size(codesIterable.iterator()));
 			subProgress.start();
 
 			Iterator<? extends Code> codes = codesIterable.iterator();
 
 			if (!codes.hasNext()) {
 				newFilterList = new TreeSet<PrimaryDocument>();
-				CollectionUtils.addAll(newFilterList, PrimaryDocument.getTreeWalker(project.getPrimaryDocuments()).iterator());
+				CollectionUtils.addAll(newFilterList, PrimaryDocument
+						.getTreeWalker(project.getPrimaryDocuments())
+						.iterator());
 			} else {
 				Code c = codes.next();
 				String nextSearchString = c.getTag();
-				
-				if (nextSearchString.startsWith("^") || nextSearchString.startsWith("-")) {
+
+				if (nextSearchString.startsWith("^")
+						|| nextSearchString.startsWith("-")) {
 					newFilterList = new TreeSet<PrimaryDocument>();
-					CollectionUtils.addAll(newFilterList, PrimaryDocument.getTreeWalker(project.getPrimaryDocuments()).iterator());
+					CollectionUtils.addAll(newFilterList, PrimaryDocument
+							.getTreeWalker(project.getPrimaryDocuments())
+							.iterator());
 					newFilterList.removeAll(getSetFromSearchString(c, project));
 				} else {
-					newFilterList = new TreeSet<PrimaryDocument>(getSetFromSearchString(c,
-						project));
+					newFilterList = new TreeSet<PrimaryDocument>(
+							getSetFromSearchString(c, project));
 				}
 
 				if (subProgress.isCanceled())
@@ -106,18 +113,22 @@ public class SearchService {
 					nextSearchString = c.getTag();
 
 					// Remove from hits if starting with - or ^
-					if (nextSearchString.startsWith("^") || nextSearchString.startsWith("-")) {
-						newFilterList.removeAll(getSetFromSearchString(c, project));
+					if (nextSearchString.startsWith("^")
+							|| nextSearchString.startsWith("-")) {
+						newFilterList.removeAll(getSetFromSearchString(c,
+								project));
 					} else {
-						if (nextSearchString.startsWith("|")){
-							
-							Collection<PrimaryDocument> nextSet = getSetFromSearchString(c, project);
-							
+						if (nextSearchString.startsWith("|")) {
+
+							Collection<PrimaryDocument> nextSet = getSetFromSearchString(
+									c, project);
+
 							nextSet.removeAll(newFilterList);
-							
+
 							newFilterList.addAll(nextSet);
 						} else {
-							newFilterList.retainAll(getSetFromSearchString(c, project));
+							newFilterList.retainAll(getSetFromSearchString(c,
+									project));
 						}
 					}
 
@@ -135,43 +146,46 @@ public class SearchService {
 
 		return new ArrayList<PrimaryDocument>(newFilterList);
 	}
-	
-	public static String cleanStringForSearch(String s){
-		
+
+	public static String cleanStringForSearch(String s) {
+
 		s = s.trim();
-		
-		if (s.startsWith("\"") && s.endsWith("\"")){
+
+		if (s.startsWith("\"") && s.endsWith("\"")) {
 
 			// Remove leading/trailing double quotes
 			s = s.substring(1, s.length() - 1);
-			
-			// Unescape 			
+
+			// Unescape
 			s = StringEscapeUtils.unescapeJava(s);
 		}
-		
+
 		return s;
 	}
 
-	public Collection<PrimaryDocument> getSetFromSearchString(Code c, Project project) {
+	public Collection<PrimaryDocument> getSetFromSearchString(Code c,
+			Project project) {
 
 		String nextSearchString = c.getTag();
 
 		char operation = '+';
 
-		if (nextSearchString.startsWith("^") || nextSearchString.startsWith("-") || nextSearchString.startsWith("|")) {
+		if (nextSearchString.startsWith("^")
+				|| nextSearchString.startsWith("-")
+				|| nextSearchString.startsWith("|")) {
 			operation = nextSearchString.charAt(0);
 			nextSearchString = nextSearchString.substring(1);
 		}
 
-		if ("date".equals(nextSearchString) && c.getValue() != null){
-			
+		if ("date".equals(nextSearchString) && c.getValue() != null) {
+
 			String searchString = c.getValue().trim();
-			
+
 			if (searchString.startsWith("<") || searchString.startsWith(">")) {
 				operation = searchString.charAt(0);
 				searchString = searchString.substring(1);
 			}
-			
+
 			DateTime compareAgainst;
 			try {
 				compareAgainst = new DateTime(searchString);
@@ -179,65 +193,67 @@ public class SearchService {
 				log.error("Could not parse DateTime");
 				return Collections.emptyList();
 			}
-			
+
 			LinkedList<PrimaryDocument> result = new LinkedList<PrimaryDocument>();
 
 			int n = 0;
 			int i = 0;
-			
-			for (PrimaryDocument doc : PrimaryDocument.getTreeWalker(project.getPrimaryDocuments())) {
-				
+
+			for (PrimaryDocument doc : PrimaryDocument.getTreeWalker(project
+					.getPrimaryDocuments())) {
+
 				n++;
-				
+
 				String date = doc.getMetaData("date");
-				if (date != null){
+				if (date != null) {
 					DateTime time;
 					try {
 						time = new DateTime(date);
 					} catch (IllegalArgumentException e) {
 						continue;
 					}
-					if (operation == '<'){
-						if (time.isBefore(compareAgainst)){
+					if (operation == '<') {
+						if (time.isBefore(compareAgainst)) {
 							result.add(doc);
 							i++;
 						}
 					}
-					if (operation == '>'){
-						if (time.isAfter(compareAgainst)){
+					if (operation == '>') {
+						if (time.isAfter(compareAgainst)) {
 							result.add(doc);
 							i++;
 						}
 					}
-					
+
 				}
 			}
 			System.out.println(n + " " + i);
 			return result;
-			
-		} else 
-		
+
+		} else
+
 		// Search using Lucene if code is "search"
 		if ("search".equals(nextSearchString) && c.getValue() != null
-			&& c.getValue().trim().length() > 1) {
-			
+				&& c.getValue().trim().length() > 1) {
+
 			String searchString;
-			
+
 			List<? extends Code> props = c.getProperties();
-			
-			if (props.size() == 1 && props.get(0).getTag().equals("desc")){
+
+			if (props.size() == 1 && props.get(0).getTag().equals("desc")) {
 				searchString = cleanStringForSearch(c.getValue());
 			} else {
 				StringJoiner sb = new StringJoiner(" ");
-				for (Code sub : props){
+				for (Code sub : props) {
 					sb.append(cleanStringForSearch(sub.toString()));
 				}
 				searchString = sb.toString();
 			}
 
 			searchStringProxy.setVariable(searchString);
-			
-			Iterator<PrimaryDocument> it = luceneFacade.search(project, searchString);
+
+			Iterator<PrimaryDocument> it = luceneFacade.search(project,
+					searchString);
 			LinkedList<PrimaryDocument> result = new LinkedList<PrimaryDocument>();
 			CollectionUtils.addAll(result, it);
 
@@ -246,14 +262,15 @@ public class SearchService {
 			if ("*".equals(nextSearchString)) {
 
 				Set<PrimaryDocument> result = new HashSet<PrimaryDocument>();
-				for (PrimaryDocument doc : project.getCodeModel().getPrimaryDocuments()) {
+				for (PrimaryDocument doc : project.getCodeModel()
+						.getPrimaryDocuments()) {
 					if (doc.getParent() != null)
 						result.add(doc);
 				}
 				return result;
 			} else {
-				List<PrimaryDocument> results = project.getCodeModel().getPrimaryDocuments(
-					nextSearchString);
+				List<PrimaryDocument> results = project.getCodeModel()
+						.getPrimaryDocuments(nextSearchString);
 
 				switch (operation) {
 				case '-':
@@ -266,7 +283,7 @@ public class SearchService {
 					List<PrimaryDocument> filteredResults = new ArrayList<PrimaryDocument>();
 					for (PrimaryDocument result : results) {
 						if (CodedStringFactory.parse(result.getCodeAsString())
-							.containsAny(nextSearchString))
+								.containsAny(nextSearchString))
 							filteredResults.add(result);
 					}
 					return filteredResults;
